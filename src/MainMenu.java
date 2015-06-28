fb2975112c41758b690ba09c789f8610c27ae2a3
lczornyj <lee.czornyj@netbuilder.com>
2015-06-28 19:50:50 +0100

    another edit

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -20,7 +20,7 @@ import java.util.Scanner;
  */
 public class MainMenu {
 /**
- * The inital 
+ * The inital declaration of the objects in the main method along with any variables. 
  */
 	private DisplayHelp displayHelp = new DisplayHelp();
 	private AddProduct add = new AddProduct();
@@ -29,13 +29,22 @@ public class MainMenu {
 	private ArrayList<Product> products = new ArrayList<Product>();
 	private String nameOfFile;
 	private String fileLocation;
-	private int numberGenerator;
+	
+	/**
+	 * The main method which is the central controller of the IMS created.
+	 * This contains all commands used to initialise the programme along with the construction
+	 * of the IMS.
+	 * The main menu with the various commands are then accessed through the menuOptions method.
+	 */
 	
 	public MainMenu() {
 	displayHelp.displayMenuItems();
 		menuOptions();
 		
-
+/**
+ * Determines the files location and name using user inputs.
+ * Then the list of products are output at the end of the inputs.
+ */
 		System.out.print("Enter the files location: ");
 		Scanner in = new Scanner(System.in);
 		fileLocation = in.nextLine().trim();
@@ -48,9 +57,14 @@ public class MainMenu {
 		{
 			System.out.println(product.getName() + ", " + product.getproductid() + ", " + product.getStock());			
 		}
-		//calculations(numberGenerator);
 		numberGenerator();
 	}
+	/**
+	 * The main menu options. This contains a user input at the start which then activates
+	 * a particular method. This then loops round back to the main menu so that another
+	 * method can be used. If at any point the user types in "End" the loop is broken and the
+	 * main menu is terminated.
+	 */
 	private void menuOptions() {
 		@SuppressWarnings("resource")
 		Scanner in = new Scanner(System.in);
@@ -80,14 +94,18 @@ public class MainMenu {
 				break;	
 		}
 	}
-	
-		
+	/**
+	 *  This exports the data from the array list to a text file based on what the
+	 *  user has called the file. At the end of the text file, will be a date when 
+	 *  the list was created.
+	 */
 		Calendar rightNow = Calendar.getInstance();
 	public void fileWritingMethod(File outputfile) {
 	try {
 		DataOutputStream da = new DataOutputStream(new FileOutputStream(outputfile));
+		da.writeUTF("Product   ID   Stock level \r\n");
 		for (int i = 0; i < products.size(); ++i){
-			da.writeUTF(products.get(i).getName() + " " + products.get(i).getproductid() + " " + products.get(i).getStock() + "\r\n");
+			da.writeUTF(products.get(i).getName() + "     " + products.get(i).getproductid() + "       " + products.get(i).getStock() + "\r\n");
 			}
 		da.writeUTF(rightNow.getTime().toLocaleString());
 		da.close();
@@ -95,15 +113,15 @@ public class MainMenu {
 	catch(IOException e){
 		}
 	}
-	
+	/**
+	 * This is a random number generator to simulate the decrement of the stock
+	 * level. If at any point a products stock level gets below 15, the method
+	 * then outputs a warning.
+	 */
 	public void numberGenerator (){
 		Random generator = new Random();
 		int randomDecrease = generator.nextInt(10)+1;
-		System.out.println(randomDecrease);
-		
-		// select random product based of integer
-		
-		
+		// select random product based of random integer selected and take product id from that number
 		int maxvalue = products.size();
 		Random stockgenerator = new Random();
 		int stocknumber = stockgenerator.nextInt(maxvalue);
@@ -112,15 +130,18 @@ public class MainMenu {
 		if(testLimits <= 0){
 			adjustments = 0;
 		}	
-		else {
-		// needs work to find the right value and decrease, as well as have it running during programme.
+		else { 
 			adjustments = adjustments - randomDecrease;
 		}
 		products.get(stocknumber).setStock(adjustments);
-		
-		
-		
+		if (products.get(stocknumber).getStock() < 15){
+			//System.out.println("the product" + (products.getName() + ", ID number " + product.getproductid() + ", has stock of " + product.getStock() + "Reorder this product");
+			System.out.println("test");
+		}
 	}
+	
+	
+	// work in progress for decrement along side the main method.
 	public static void calculations(int numberGenerator) {
 		
 		for (int i = 0; i<1000000000; ++i){
